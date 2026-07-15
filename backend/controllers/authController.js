566ce7b4433eeddc92ac5627ada0aa86ebb9e1dd
Nidhi875 <nidhi2437@gmail.com>
2026-07-16 00:12:59 +0530

    Changed gmail smtp

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -146,14 +146,20 @@ exports.forgotPassword = async (req, res) => {
     const resetLink =
       `${process.env.FRONTEND_URL}/reset-password.html?token=${resetToken}`;
 
-    const transporter =
-      nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.EMAIL_USER,
-          pass: process.env.EMAIL_PASS
-        }
-      });
+
+      const transporter =
+  nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
